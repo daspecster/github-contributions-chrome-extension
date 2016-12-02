1d6b76d1f17dc36d5ac166fb8db6a2a43b70ea98
Austin Kregel <austin@kbcomp.co>
2016-12-02 01:17:08 -0500

    Code style changes and fix spelling mistakes

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,14 @@
-username = "";
+var username = "";
 
 /**
  * Grab the user from github
  * Then parse the repsonse html with the DOMParser to extract the username
- * @param Closure callback if it's set, it will be executed after the username is retrieved.
+ * @param callback if it's set, it will be executed after the username is retrieved.
  */
 function get_username(callback) {
     var url = "https://github.com/";
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
+    xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
             var parser = new DOMParser();
             var dom = parser.parseFromString(xhr.responseText, "text/html");
@@ -29,47 +29,47 @@ function parse_username(content) {
     username = user_meta[0].getAttribute('content');
 }
 /**
- * This method will create a canvas later to be used as the icon in 
+ * This method will create a canvas later to be used as the icon in
  * the chrome menu. Once created, it will set the image data for chrome.
- * @param hex color A hexadecimal color formated in hex.
- * @param int contributions The number of contributions for a given day.
+ * @param color A hexadecimal color formatted in hex.
+ * @param contributions The number of contributions for a given day.
  */
 function draw(color, contributions) {
-  var canvas = document.createElement('canvas');
-  canvas.width = 19;
-  canvas.height = 19;
+    var canvas = document.createElement('canvas');
+    canvas.width = 19;
+    canvas.height = 19;
 
-  var context = canvas.getContext('2d');
-  context.fillStyle = color;
-  context.fillRect(0, 0, 19, 19);
+    var context = canvas.getContext('2d');
+    context.fillStyle = color;
+    context.fillRect(0, 0, 19, 19);
 
-  color_hex = parseInt("0x" + color.slice(1));
-  if (color_hex < 0x44a340) {
-      text_color = "#FFFFFF";
-  } else {
-      text_color = "#000000";
-  }
-  context.fillStyle = text_color;
-  context.textAlign = "center";
-  context.textBaseline = "middle";
-  context.font = "11px Arial";
-  context.fillText(contributions, 10, 10);
+    color_hex = parseInt("0x" + color.slice(1));
+    if (color_hex < 0x44a340) {
+        text_color = "#FFFFFF";
+    } else {
+        text_color = "#000000";
+    }
+    context.fillStyle = text_color;
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.font = "11px Arial";
+    context.fillText(contributions, 10, 10);
 
-  chrome.browserAction.setIcon({
-    imageData: context.getImageData(0, 0, 19, 19)
-  });
+    chrome.browserAction.setIcon({
+        imageData: context.getImageData(0, 0, 19, 19)
+    });
 }
 
 /**
  * Handle the contributions data by parsing through the temporary dom
- * Then calls draw to build the icon for the extention icon
- * @param DOM response_dom
+ * Then calls draw to build the icon for the extension icon
+ * @param response_dom
  */
 function handle_contrib_data(response_dom) {
     var weeks = response_dom.getElementsByTagName("g");
-    weeks = weeks[weeks.length-1];
+    weeks = weeks[weeks.length - 1];
     var days = weeks.getElementsByTagName("rect");
-    var last_day = days[days.length-1];
+    var last_day = days[days.length - 1];
 
     var todays_contributions = last_day.getAttribute("data-count");
     var color = last_day.getAttribute("fill");
@@ -77,13 +77,13 @@ function handle_contrib_data(response_dom) {
 }
 
 /**
- * Get the contrbutions for the given user and parse the data 
+ * Get the contrbutions for the given user and parse the data
  */
 function get_contrib() {
     console.log(username);
     var contrib_url = "https://github.com/users/" + username + "/contributions";
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
+    xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
             var parser = new DOMParser();
             var dom = parser.parseFromString(xhr.responseText, "text/html");
